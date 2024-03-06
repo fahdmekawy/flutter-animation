693fef903c03321cd5c4e9004ebe055726613da4
Fahd Mekawy <40622301+fahdmekawy@users.noreply.github.com>
2024-03-06 20:35:22 +0200

    - added animated container widget with its curves

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animation/widgets/animated_container.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          color: Colors.black,
+          titleTextStyle: TextStyle(
+            fontWeight: FontWeight.w600,
+            color: Colors.white,
+            fontSize: 24,
+          ),
+        ),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Animations'),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            AnimatedContainerWidget(),
+          ],
+        ),
+      ),
+    );
   }
 }
